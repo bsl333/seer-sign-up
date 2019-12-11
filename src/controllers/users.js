576726201767ba82857595aec73f939c9945b48c
Branden <b.lowe333@gmail.com>
2019-12-11 14:25:42 -0800

    code cleanup, eslint allow console.error messages

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,17 +1,13 @@
 const User = require('../models/user');
 
 async function create (req, res, next) {
-  console.log(req.body);
-  const { firstName, lastName, email, username, password } = req.body;
   try {
     const user = new User(req.body);
-    console.log(user);
     await user.save();
     res.status(201).send(user.toJSON());
   } catch (e) {
     res.status(400).send(e.message);
   }
-  console.log(firstName, lastName, email, username, password);
 }
 
 module.exports = {
