7c983ed16955a93f902f52913ee3e6e3109a967d
Branden <b.lowe333@gmail.com>
2019-12-11 11:52:54 -0800

    added eslint and fixed linting errors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
