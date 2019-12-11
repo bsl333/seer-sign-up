f8b908215a477293d4fa260799749a45b9fe5824
Branden <b.lowe333@gmail.com>
2019-12-11 11:40:06 -0800

    project layout and express setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+module.exports = app;
