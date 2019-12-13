a3b85bbcf039fc2507fad1ce12c5f38fe4e59628
Branden <b.lowe333@gmail.com>
2019-12-12 19:16:16 -0800

    added ability to check if email or username exists

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 
-async function create (req, res, next) {
+async function create(req, res) {
   try {
     const user = new User(req.body);
     await user.save();
@@ -10,6 +10,22 @@ async function create (req, res, next) {
   }
 }
 
+async function checkEmail(req, res) {
+  const email = req.params.email;
+  const emailTaken = await User.findOne({ email });
+  res.status(200).send({
+    available: !emailTaken
+  });
+}
+
+async function checkUsername(req, res) {
+  const username = req.params.username;
+  const usernameTaken = await User.findOne({ username });
+  res.status(200).send({
+    available: !usernameTaken
+  });
+}
+
 module.exports = {
-  create
+  create, checkEmail, checkUsername
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const usersCtrl = require('../controllers/users');
 
 router.post('/', usersCtrl.create);
+router.get('/check/email/:email', usersCtrl.checkEmail);
+router.get('/check/username/:username', usersCtrl.checkUsername);
 
 module.exports = router;
